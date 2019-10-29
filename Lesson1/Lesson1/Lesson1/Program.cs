aa5e77e2685ea302de9be2d8f716e6d32fe8a7ff
Dmitry Ipatov <dimaipatov@gmail.com>
2019-10-29 21:32:52 +0300

    First update

---
+++ Lesson1/Lesson1/Lesson1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Thread.Sleep(3000);
+            Console.WriteLine("Привет, {0}, {1}, {2}, {3}", name, "Maria", "Vladislav", "Ivan");
+            Console.ReadKey();
+        }
+    }
+}
