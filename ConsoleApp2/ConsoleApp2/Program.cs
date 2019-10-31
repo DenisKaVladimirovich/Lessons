359f26c7a1f2459301da95c981752b5e46e3237e
it_darya_trade <it@darya-trade.ru>
2019-10-31 09:26:16 +0300

    Ok

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввежите имя");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine("Hello, {0}", name);
+            Thread.Sleep(5000);
+            Console.Write("Goodby");
+            Console.ReadKey();
+        }
+    }
+}
